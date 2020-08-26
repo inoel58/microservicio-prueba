bfb1d50e5552b063ef21ea1545423d61b305c19a
Inoel <Inoel@DESKTOP-FTUMDDC>
2020-08-27 00:03:28 +0200

    Primera prueba docker

--- src/main/java/ino/practicas/App.java
+++ src/main/java/ino/practicas/App.java
@@ -1,13 +1,21 @@
 package ino.practicas;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+  @RequestMapping("/")
+  public String home() {
+    return "Hello Docker World";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+
 }
